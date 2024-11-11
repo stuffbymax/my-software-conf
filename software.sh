327a52c0b0dde2e1e03d2a935c324edeac2287b5
stuffbymax <96355058+stuffbymax@users.noreply.github.com>
2024-11-11 13:28:16 +0000

    Update software.sh

--- software.sh
+++ software.sh
@@ -12,12 +12,23 @@ sudo pacman -Syu --noconfirm || handle_error
 
 # List of software to install (UFW, Git, Krita, and Python-related packages)
 software=(
-    "git"                    # Version control system
-    "krita"                  # Digital painting software
-    "python"                 # Python programming language
-    "python-pip"             # Python package installer
-    "python-virtualenv"      # Package for creating virtual environments
-    "ufw"                    # Uncomplicated Firewall
+    "git"                   
+    "krita"                 
+    "python"                 
+    "python-pip"            
+    "python-virtualenv"      
+    "vim"
+    "htop"
+    "curl"
+    "wget"
+    "python"
+    "python-pip"
+    "python-virtualenv"
+    "mpv"
+    "filezilla"
+    "thunar"
+    "geany"
+    "spyder"
 )
 
 # Install software
@@ -35,35 +46,3 @@ done
 echo "Cleaning up orphaned packages..."
 sudo pacman -Rns $(pacman -Qdtq) || handle_error
 
-# Set up UFW
-echo "Setting up UFW..."
-
-# Allow SSH connections (port 22)
-sudo ufw allow ssh
-
-# Allow common web server ports
-sudo ufw allow http   # Port 80
-sudo ufw allow https  # Port 443
-
-# Allow FTP (port 21)
-sudo ufw allow ftp    # FTP Control Port
-
-# Allow FTP Data Port (Passive Mode)
-sudo ufw allow 20     # FTP Data Port (Passive Mode)
-
-# Allow other common services
-sudo ufw allow 3306    # MySQL
-sudo ufw allow 5432    # PostgreSQL
-sudo ufw allow 6379    # Redis
-sudo ufw allow 27017   # MongoDB
-sudo ufw allow 8080    # HTTP Alternative (commonly used for web applications)
-
-# Enable UFW
-sudo ufw enable || handle_error
-
-# Show the status of UFW
-echo "UFW status:"
-sudo ufw status verbose
-
-# Notify user for further customization
-echo "Installation and UFW setup complete!"
